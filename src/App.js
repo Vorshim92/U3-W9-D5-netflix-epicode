2299aa18a1217cf1df53a91dd9fa2b86c873892f
Vorshim92 <vorshim92@gmail.com>
2024-03-15 15:38:28 +0100

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ function App() {
         <Carousel title={"Top Rated"} type={urls.fetchTopRated}></Carousel>
         <Carousel title={"Popular"} type={urls.fetchPopular}></Carousel>
         <Carousel title={"TV Shows"} type={urls.fetchTVShows}></Carousel>
+        <Carousel title={"Comedy"} type={urls.fetchComedy}></Carousel>
+        <Carousel title={"Action"} type={urls.fetchAction}></Carousel>
+        <Carousel title={"Documentaries"} type={urls.fetchDocumentaries}></Carousel>
+        <Carousel title={"Horror"} type={urls.fetchHorror}></Carousel>
       </main>
       <footer>
         <Footer></Footer>
