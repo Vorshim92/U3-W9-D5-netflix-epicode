6cc77d1934cd2604b2d24a31c388380d818ffdec
Vorshim92 <vorshim92@gmail.com>
2024-03-15 17:24:19 +0100

    added SPINNER EXTRA and CAROUSEL JS

--- src/component/carousel/carousel-nohook.jsx
+++ src/component/carousel/carousel-nohook.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import routes from "../../routes";
 import { options } from "../../options/fetchOptions.js";
+import { Spinner } from "react-bootstrap";
 
 class CarouselClass extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class CarouselClass extends Component {
 
     this.state = {
       movies: [],
+      isLoading: true,
     };
   }
   componentDidMount() {
@@ -16,7 +18,7 @@ class CarouselClass extends Component {
       try {
         const response = await fetch(this.props.type, options);
         const data = await response.json();
-        this.setState({ movies: data.results });
+        this.setState({ movies: data.results, isLoading: false });
       } catch (error) {
         console.error(error);
       }
@@ -28,59 +30,63 @@ class CarouselClass extends Component {
     return (
       <>
         <div className="container-fluid carosello">
-          <div className="container-indicators d-none">
+          <div className="container-indicators">
             <div className="indicator active" data-index="0"></div>
             <div className="indicator" data-index="1"></div>
             <div className="indicator" data-index="2"></div>
           </div>
 
           <h3 className="text-white mt-2">{this.props.title}</h3>
-          <div className="test">
-            <button type="button" className="btn-nav moveLeft">
-              ᐊ
-            </button>
-            <button type="button" className="btn-nav moveRight">
-              ᐅ
-            </button>
-            <div className="slider" id="mySlider">
-              {this.state.movies.map((movie, i) => (
-                <div className="movie" id="trending" key={movie.id}>
-                  <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
-                  <div className="description">
-                    <div className="description__buttons-container">
-                      <div className="description__button">
-                        <i className="fas fa-play"></i>
+          {this.state.isLoading ? (
+            <Spinner animation="border" variant="danger" />
+          ) : (
+            <div className="test">
+              <button type="button" className="btn-nav moveLeft">
+                ᐊ
+              </button>
+              <button type="button" className="btn-nav moveRight">
+                ᐅ
+              </button>
+              <div className="slider" id="mySlider">
+                {this.state.movies.map((movie, i) => (
+                  <div className="movie" id="trending" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+                    <div className="description">
+                      <div className="description__buttons-container">
+                        <div className="description__button">
+                          <i className="fas fa-play"></i>
+                        </div>
+                        <div className="description__button">
+                          <i className="fas fa-plus"></i>
+                        </div>
+                        <div className="description__button">
+                          <i className="fas fa-thumbs-up"></i>
+                        </div>
+                        <div className="description__button">
+                          <i className="fas fa-thumbs-down"></i>
+                        </div>
+                        <div className="description__button">
+                          <i className="fas fa-chevron-down"></i>
+                        </div>
                       </div>
-                      <div className="description__button">
-                        <i className="fas fa-plus"></i>
+                      <div className="description__text-container">
+                        <span className="description__match">97% Match</span>
+                        <span className="description__rating">TV-14</span>
+                        <span className="description__length">2h 11m</span>
+                        <br />
+                        <br />
+                        <span>Explosive</span>
+                        <span>&middot;</span>
+                        <span>Exciting</span>
+                        <span>&middot;</span>
+                        <span>Family</span>
                       </div>
-                      <div className="description__button">
-                        <i className="fas fa-thumbs-up"></i>
-                      </div>
-                      <div className="description__button">
-                        <i className="fas fa-thumbs-down"></i>
-                      </div>
-                      <div className="description__button">
-                        <i className="fas fa-chevron-down"></i>
-                      </div>
-                    </div>
-                    <div className="description__text-container">
-                      <span className="description__match">97% Match</span>
-                      <span className="description__rating">TV-14</span>
-                      <span className="description__length">2h 11m</span>
-                      <br />
-                      <br />
-                      <span>Explosive</span>
-                      <span>&middot;</span>
-                      <span>Exciting</span>
-                      <span>&middot;</span>
-                      <span>Family</span>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </>
     );
